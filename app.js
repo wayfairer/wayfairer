9b8268cbfc65fe4864cf5742d6d47f592ce8f7ed
oli_hoots <oliabc4@gmail.com>
2015-07-29 22:58:11 +1000

    changed muter empty

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(multer({ dest: './tmp/'}))
+app.use(multer({}));
 
 app.use(expressValidator());
 app.use(methodOverride());
