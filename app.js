cf2aa85e8564b68566df0bb7ca349bdd0fbcf393
oli_hoots <oliabc4@gmail.com>
2015-07-29 23:34:28 +1000

    lol

--- app.js
+++ app.js
@@ -84,7 +84,21 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(multer());
+app.use(multer({ 
+    dest: './logs/',
+    rename: function (fieldname, filename) {
+        return filename.replace(/\W+/g, '-').toLowerCase() + Date.now()
+    },
+    onFileUploadStart: function (file) {
+        console.log(file.fieldname + ' is starting ...')
+    },
+    onFileUploadData: function (file, data) {
+        console.log(data.length + ' of ' + file.fieldname + ' arrived')
+    },
+    onFileUploadComplete: function (file) {
+        console.log(file.fieldname + ' uploaded to  ' + file.path)
+    }
+}));
 
 app.use(expressValidator());
 app.use(methodOverride());
--- config/secrets.js
+++ config/secrets.js
@@ -18,8 +18,8 @@
 
 module.exports = {
 
-  db: process.env.MONGOLAB_URI || process.env.MONGODB || 'mongodb://heroku_app32722305:wayfairer2015!@ds027771.mongolab.com:27771/heroku_app32722305',
-  //db: process.env.MONGOLAB_URI || process.env.MONGODB || 'mongodb://localhost:27017/wayfairer',
+  //db: process.env.MONGOLAB_URI || process.env.MONGODB || 'mongodb://heroku_app32722305:wayfairer2015!@ds027771.mongolab.com:27771/heroku_app32722305',
+  db: process.env.MONGOLAB_URI || process.env.MONGODB || 'mongodb://localhost:27017/wayfairer',
 
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
 
