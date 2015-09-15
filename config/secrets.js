31f1969242c7fad63a63ac202ac76efaa5d04aee
unknown <wayfairers.co@gmail.com>
2015-09-15 17:40:25 +1000

    review the configuration in secret.js

--- config/secrets.js
+++ config/secrets.js
@@ -18,7 +18,7 @@
 
 module.exports = {
 
-  // db: process.env.MONGOLAB_URI || process.env.MONGODB || 'mongodb://heroku_app32722305:wayfairer2015!@ds027771.mongolab.com:27771/heroku_app32722305',
+  // db: process.env.MONGOLAB_URI || process.env.MONGODB || 'mongodb://heroku_5bsnmnhq:cgud47ouj8ulju9td7k0ecjlap@ds041613.mongolab.com:41613/heroku_5bsnmnhq',
   db: process.env.MONGOLAB_URI || process.env.MONGODB || 'mongodb://localhost:27017/wayfairer',
 
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
